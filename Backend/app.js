4557ceee3ab71e1f414ad2e3ef9639467a03b909
Marlène D <marlene.a.diaz@gmail.com>
2023-06-22 13:17:06 +0200

    Explain corp error // browser

--- Backend/app.js
+++ Backend/app.js
@@ -39,7 +39,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); // Prevent  cors error blocking img
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin'); // Prevent  cors error blocking img 
+    //- The error stems from a security mechanism that browsers implement = same-origin policy.
     next();
   });
 
