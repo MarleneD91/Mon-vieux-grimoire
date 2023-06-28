e679cd77afed8f32cc874912481bd4176feb842b
Marlène D <marlene.a.diaz@gmail.com>
2023-06-28 12:17:33 +0200

    Change comment value

--- Backend/middlewares/signup-login-account-limiter.js
+++ Backend/middlewares/signup-login-account-limiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const createAccountLimiter = (req, res, next) => {
     rateLimit({
         windowMs: 30 * 60 * 1000, // 30min
-        max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+        max: 5, // Limit each IP to 5 create account requests per `window` (here, per half hour)
         message:
             'Too many connexion &/or account creation tenants from this IP, please try again after half an hour',
         standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
